096bd1dfb4816ff529e21bcc8491f6e979bd9929
capricorn <8250820+capricorn@users.noreply.github.com>
2023-12-03 08:39:25 -0700

    Problem 3 part 2 solution

--- p3.py
+++ p3.py
@@ -1,7 +1,8 @@
 import re
+#from math import prod
 
-def read_input():
-    with open('p3.txt', 'r') as f:
+def read_input(filename='p3.txt'):
+    with open(filename, 'r') as f:
         return [ line.strip() for line in f.readlines() ]
     
 def p3p1():
@@ -25,4 +26,31 @@ def p3p1():
     
     return sum(parts)
 
-print(p3p1())
+def p3p2():
+    col=140
+    puzzle = [ '.'*col, *read_input(), '.'*col ]
+
+    parts = []
+    for i in range(1, len(puzzle)-1):
+        top = re.finditer('\d+', puzzle[i-1])
+        middle = re.finditer('\d+', puzzle[i])
+        bottom = re.finditer('\d+', puzzle[i+1])
+
+        adj = [ *top, *middle, *bottom ]
+        stars = re.finditer('\*', puzzle[i])
+
+        for star in stars:
+            star_start, star_end = star.start(0), star.end(0)
+
+            matches = []
+            for adj_match in adj:
+                adj_start, adj_end = adj_match.start(0), adj_match.end(0)
+                if star_start >= adj_start-1 and star_end <= adj_end+1:
+                    matches.append(int(adj_match.group(0)))
+            
+            if len(matches) >= 2:
+                parts.append(matches)
+
+    return sum([ p1*p2 for p1,p2 in parts ])
+
+print(p3p2())
