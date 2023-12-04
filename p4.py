292ee44248bd5b5468b1c9de83bc9259de534f39
capricorn <8250820+capricorn@users.noreply.github.com>
2023-12-04 09:06:51 -0700

    Problem 4 part 1 solution

--- p4.py
+++ p4.py
@@ -1,4 +1,23 @@
+import re
+
 def read_input(filename='p4.txt'):
     with open(filename, 'r') as f:
         return [ line.strip() for line in f.readlines() ]
 
+
+def p4p1():
+    cards = read_input(filename='p4.txt')
+    ylen = 10
+
+    card_tuples = list(map(lambda c: (c[1:ylen+1], c[ylen+1:]), [ re.findall('\d+', card) for card in cards ]))
+    points = []
+    for your_cards, winners in card_tuples:
+        print(your_cards, winners, set(your_cards).intersection(set(winners)))
+        # Are they always unique?
+        matches = len(set(your_cards).intersection(set(winners)))
+        points.append(0 if matches == 0 else 2**(matches-1))
+        print(points[-1])
+
+    return sum(points)
+
+print(p4p1())
